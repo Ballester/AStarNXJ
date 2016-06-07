0ff07c4096dd36d744d06a8e6892aef30ed23cfe
Pedro Ballester <Pedro Ballester>
2016-06-07 11:11:35 -0300

    first commit

---
+++ src/AStar.java
@@ -0,0 +1,19 @@
+
+import lejos.nxt.Button;
+import lejos.nxt.Motor;
+
+public class AStar {
+	public static void main(String[] args) {
+//		System.out.println("noia");
+//		System.out.println("noia2");
+		Motor.A.rotate(720, true);
+		Motor.B.rotate(720, true);
+		Motor.C.rotate(90);
+		Motor.C.rotate(-180);
+		//		Button.waitForAnyPress();
+		
+//		Motor.A.forward();
+//		Motor.B.forward();
+		
+	}
+}
